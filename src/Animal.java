99452fabaa52fa49bd0402e99771184bea19a2df
savincev <savincev@yande.tutu>
2024-12-09 12:46:56 +0300

    Lesson 10

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    private static int animalCount = 0;
+    public Animal() {
+        animalCount++;
+    }
+    public static int getAnimalCount() {
+        return animalCount;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 
 public class Main {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
-
-        employees[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", 89556667711L, 30000, 30);
-        employees[1] = new Employee("Petrov Petr", "Manager", "petrov@mailbox.com", 89556667722L, 40000, 35);
-        employees[2] = new Employee("Sidorov Sidor", "Developer", "sidorov@mailbox.com", 89556667733L, 50000, 28);
-        employees[3] = new Employee("Smirnov Smirn", "Designer", "smirnov@mailbox.com", 89556667744L, 60000, 32);
-        employees[4] = new Employee("Kuznetsov Kuzya", "Analyst", "kuznetsov@mailbox.com", 89556667755L, 70000, 40);
-
-        for (Employee employee : employees) {
-            employee.displayInfo();
+        DOG dog1 = new DOG();
+        CAT cat1 = new CAT();
+        dog1.run(150);
+        dog1.swim(5);
+        cat1.run(100);
+        cat1.swim(10);
+        CAT[] cats = new CAT[3];
+        for (int i = 0; i < cats.length; i++) {
+            cats[i] = new CAT();
+            cats[i].setFoodBowl(10);
+            cats[i].eat(5);
+            System.out.println("Кот " + (i + 1) + " сытый: " + cats[i].isFull());
         }
-        Park amusementPark = new Park("Развлекательный Парк", 3);
-        amusementPark.addAttraction(0, "Американские горки", "10:00 - 22:00", 100);
-        amusementPark.addAttraction(1, "Колесо обозрения", "10:00 - 22:00", 200);
-        amusementPark.addAttraction(2, "Поездка на лодке", "10:00 - 20:00", 300);
-        amusementPark.displayAttractions();
+        System.out.println("Всего животных: " + Animal.getAnimalCount());
+        System.out.println("Всего собак: " + DOG.getDogCount());
+        System.out.println("Всего котов: " + CAT.getCatCount());
     }
 }
